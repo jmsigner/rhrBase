dd89d53d72d249519048f4c25cdbc72f7606ece8
jmsigner <jmsigner@gmail.com>
2015-04-01 15:21:25 +0200

    fixed test error

--- src/rhrSiteFidelity.cpp
+++ src/rhrSiteFidelity.cpp
@@ -10,16 +10,16 @@ List simpleRandomWalk(float sx, float sy, NumericVector sinA, NumericVector cosA
 
   int n = d.size();
 
-  NumericVector rx(n);
-  NumericVector ry(n);
+  NumericVector rx(n + 1);
+  NumericVector ry(n + 1);
 
   rx[0] = sx;
   ry[0] = sy;
 
-  for (int i = 1; i < n; i++) {
+  for (int i = 0; i < n; i++) {
 
-    rx[i] = rx[i-1] + cosA[i-1] * d[i];
-    ry[i] = ry[i-1] + sinA[i-1] * d[i];
+    rx[i+1] = rx[i] + cosA[i] * d[i];
+    ry[i+1] = ry[i] + sinA[i] * d[i];
 
   }
 
